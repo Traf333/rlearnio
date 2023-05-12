813b4738acde0da53abec8e26542e9cb993ff1f6
Igor Trofimov <igor.trofimov@symmetre.com>
2023-05-12 22:05:37 +0400

    check another path

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,17 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/api")]
+async fn api() -> &'static str {
+    "API World!"
+}
+
 #[shuttle_runtime::main]
 async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg
+            .service(api)
+            .service(hello_world);
     };
 
     Ok(config.into())
