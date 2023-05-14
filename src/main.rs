c7e0c2796d2ba21a847e05f31d8379fac08960b5
Igor Trofimov <igor.trofimov@symmetre.com>
2023-05-14 10:57:05 +0400

    check another path

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use actix_web::{get, web::ServiceConfig};
+mod controllers;
+
+use actix_web::{get, web::ServiceConfig, post, Responder, HttpResponse, web};
 use shuttle_actix_web::ShuttleActixWeb;
+use controllers::talents::talents_routes;
+use shuttle_secrets::SecretStore;
 
 #[get("/")]
 async fn hello_world() -> &'static str {
-    "Hello World!"
+    "Status OK"
 }
 
 #[get("/api")]
@@ -11,10 +15,17 @@ async fn api() -> &'static str {
     "API World!"
 }
 
+
+
 #[shuttle_runtime::main]
-async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn actix_web(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    println!("{:?} db", secret_store.get("DATABASE_URL"));
+
     let config = move |cfg: &mut ServiceConfig| {
         cfg
+            .service(web::scope("/talents").configure(talents_routes))
             .service(api)
             .service(hello_world);
     };
